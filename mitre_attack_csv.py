2857d574b138b194213b9232fa3aa98f8052eb3a
Toshitaka Satomi <satomi.toshitak@fujitsu.com>
2022-08-30 20:52:56 +0900

    Format the python file to flake8 rules

--- mitre_attack_csv.py
+++ mitre_attack_csv.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
+""" Generate csv files from MITRE ATT&CK STIX content
 
+    options
+    --attack_id: add mitre_attack_id column
+    --attack_version: specify ATT&CK version to use (default: 11.3)
+    --version: ouput this module version
+"""
 import re
 import csv
 import json
 import os
 import argparse
-import requests
-import sys
 from collections import defaultdict
 from typing import NewType, Any, List, Tuple, Dict, DefaultDict, Optional, cast
+import requests
+
 
 URL_PREFIX = 'https://raw.githubusercontent.com/mitre-attack/attack-stix-data/master/enterprise-attack/enterprise-attack-'
 INPUT_CACHE = 'attack.json'
@@ -28,26 +34,51 @@ Header = NewType('Header', Tuple[str, ...])
 
 
 def _load_attack() -> Optional[Attack]:
+    """ Load STIX content from local file
+
+    :returns: ATT&CK STIX JSON
+    :rtype: Optional[Attack]
+    """
     try:
-        with open(INPUT_CACHE) as f:
+        with open(INPUT_CACHE, 'r', encoding='utf-8') as f:
             return json.load(f)
-    except:
+    except IOError:
         return None
 
 
 def fetch_attack(url: str) -> Attack:
+    """ Fetch STIX content from specified URL
+
+    :param url: MITRE ATT&CK STIX URL
+    :type url: str
+    :returns: ATT&CK STIX JSON
+    :rtype: Attack
+    """
     response = requests.get(url)
     assert (response.status_code == 200), 'Failure fetching url'
     return response.json()
 
 
 def assert_for_stix(attack: Attack) -> None:
+    """ Fetch STIX content from specified URL
+
+    :param attack: ATT&CK STIX JSON
+    :type attack: Attack
+    """
     assert ('spec_version' in attack), 'Failure reading version info in JSON file'
     assert ('objects' in attack), 'Failure reading objects in JSON file'
     assert (attack['spec_version'] in ('2.0', '2.1')), 'Unsupported STIX version'
 
 
 def attack_by_type(attack: Attack) -> DefaultDict[str, Attacks]:
+    """ Divide STIX objects by type
+
+    :param attack: ATT&CK STIX JSON
+    :type attack: Attack
+    :retruns: Objects by type
+    :rtype: Attack
+    """
+    assert ('spec_version' in attack), 'Failure reading version info in JSON file'
     atttack_by_type: DefaultDict[str, Any] = defaultdict(list)
     for objects in attack['objects']:
         if 'type' in objects:
@@ -96,6 +127,13 @@ def minimd(s: str, fmt: str = 'text') -> str:
 
 
 def make_header(attacks: Attacks) -> Header:
+    """ Make CSV Header list
+
+    :param attack: ATT&CK STIX JSON
+    :type attack: Attack
+    :retruns: CSV Header List
+    :rtype: Header
+    """
     common_header = ['type', 'id', 'created', 'modified']
     if options.attack_id:
         common_header.append('mitre_attack_id')
@@ -105,6 +143,17 @@ def make_header(attacks: Attacks) -> Header:
 
 
 def get_fields(names: Header, attack: Attack) -> Attack:
+    """ Get filed data from a specified object.
+
+    If the object has external_id of ATT&CK object ID, this function add the ID.
+
+    :param names: CSV Header List
+    :type names: Header
+    :param attack: ATT&CK STIX JSON
+    :type attack: Attack
+    :retruns: CSV record
+    :rtype: Attack
+    """
     fields = {name: attack.get(name, '') for name in names}
     if options.attack_id and 'external_references' in attack:
         for r in attack['external_references']:
@@ -114,14 +163,26 @@ def get_fields(names: Header, attack: Attack) -> Attack:
 
 
 def encode(attack: Attack) -> Attack:
+    """ Encode description field
+
+    :param attack: ATT&CK STIX JSON
+    :type attack: Attack
+    :returns: CSV record
+    :rtype: Attack
+    """
     encoded_attack = {name: minimd(value) if name == 'description' else value
                       for name, value in attack.items()}
     return cast(Attack, encoded_attack)
 
 
 def save_csv(filename: str, attacks: Attacks) -> None:
+    """ Save CSV into specifed file
+
+    :param attack: CSV records
+    :type attack: Attack
+    """
     header = make_header(attacks)
-    with open(filename, 'w', newline='\n') as f:
+    with open(filename, 'w', newline='\n', encoding='utf-8') as f:
         writer = csv.DictWriter(f, header, quoting=csv.QUOTE_ALL)
         writer.writeheader()
         for attack in attacks:
@@ -129,6 +190,7 @@ def save_csv(filename: str, attacks: Attacks) -> None:
 
 
 def main() -> None:
+    """ Entry of this script """
     print(f'Fetching ATT&CK v.{options.attack_version} STIX file ...')
     url = f'{URL_PREFIX}{options.attack_version}.json'
     attack = _load_attack() or fetch_attack(url)
@@ -143,12 +205,18 @@ def main() -> None:
 
 
 def parse_args() -> argparse.Namespace:
+    """ Parse arguments of this script
+
+    :returns: arguments instance
+    :rtype: argparse.Namespace
+    """
     parser = argparse.ArgumentParser(description='Produce SDO/SRO CSV files from ATT&CK STIX')
     parser.add_argument('--attack_id', action='store_true',
                         help='add mitre_attack_id column')
     parser.add_argument('--attack_version', default=DEFAULT_ATTACK_VERSION,
                         help='specify ATT&CK version to use')
-    parser.add_argument('--version', '-V', action='version', version=os.path.basename(__file__) + ' version ' + TOOL_VERSION,
+    parser.add_argument('--version', '-V', action='version',
+                        version=os.path.basename(__file__) + ' version ' + TOOL_VERSION,
                         help='show version and exit')
     return parser.parse_args()
 
